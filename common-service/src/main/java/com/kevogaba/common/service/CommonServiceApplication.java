a1d45392d0fa709c1fae5317a89a85bd169f8299
Kevin Ogaba <kevogaba@gmail.com>
2022-02-08 16:33:52 +0300

    Remove eureka dependency from common-service

--- common-service/src/main/java/com/kevogaba/common/service/CommonServiceApplication.java
+++ common-service/src/main/java/com/kevogaba/common/service/CommonServiceApplication.java
@@ -3,6 +3,7 @@ package com.kevogaba.common.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CommonServiceApplication {
 
